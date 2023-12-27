6f932d8cef97ca185af61865d68cb53f69d2f7ad
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2023-12-27 14:43:42 +0000

    Add favicon.ico to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,6 +27,7 @@ class MyDocument extends Document {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" />
         <Head  />
         <title>MLab</title>
         <body>
