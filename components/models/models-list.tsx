97138acaf8727a488d9993543323d5753bcef83d
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2024-05-10 22:37:13 +0000

    fix: model map error, check length

--- components/models/models-list.tsx
+++ components/models/models-list.tsx
@@ -1,60 +1,59 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { ModelCard } from "./model-card";
 
-export const ModelsList = ({ filter } : { filter : string }) => {
-    const router = useRouter();
-    const { user } = useUser();
-    const [models, setModels] = useState<any[]>([]);
+export const ModelsList = ({ filter }: { filter: string }) => {
+	const router = useRouter();
+	const { user } = useUser();
+	const [models, setModels] = useState<any[]>([]);
 
-    useEffect(() => {
-        try {
-					fetch(
-						`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/models?user_id=${user?.email}`
-					)
-						.then((res) => res.json())
-						.then((data) => setModels(data));
-				} catch (error) {
-					console.error("Failed to fetch models", error);
-					setModels([]);
-				}
-    }, [user]);
+	useEffect(() => {
+		try {
+			fetch(
+				`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/models?user_id=${user?.email}`
+			)
+				.then((res) => res.json())
+				.then((data) => setModels(data));
+		} catch (error) {
+			console.error("Failed to fetch models", error);
+			setModels([]);
+		}
+	}, [user]);
 
-    useEffect(() => {
-        if (filter === "") return;
-        const filteredModels = models.filter((model) => {
-            // Filter by model name
-            if (model.name.toLowerCase().includes(filter.toLowerCase()))
-                return true;
-        });
-        setModels(filteredModels);
-    }, [filter, models]);
+	useEffect(() => {
+		if (filter === "") return;
+		const filteredModels = models.filter((model) => {
+			// Filter by model name
+			if (model.name.toLowerCase().includes(filter.toLowerCase())) return true;
+		});
+		setModels(filteredModels);
+	}, [filter, models]);
 
-    return (
-			<div>
-				<br />
-				<div className="flex flex-row justify-start flex-wrap gap-4">
-					{models.length > 0 ? (
-						models.map((model) => {
-							return (
-								<ModelCard
-									key={model.id}
-									owner={model.owner_id}
-									name={model.name}
-									lastUpdated={model.modified}
-									isPrivate={model.private}
-									description={model.description}
-									version={model.version}
-								/>
-							);
-						})
-					) : (
-						<div className="text-center w-full">
-							<h2>No models found</h2>
-						</div>
-					)}
-				</div>
+	return (
+		<div>
+			<br />
+			<div className="flex flex-row justify-start flex-wrap gap-4">
+				{!!models ? (
+					models.map((model) => {
+						return (
+							<ModelCard
+								key={model.id}
+								owner={model.owner_id}
+								name={model.name}
+								lastUpdated={model.modified}
+								isPrivate={model.private}
+								description={model.description}
+								version={model.version}
+							/>
+						);
+					})
+				) : (
+					<div className="text-center w-full">
+						<h2>No models found</h2>
+					</div>
+				)}
 			</div>
-		);
-}
+		</div>
+	);
+};
