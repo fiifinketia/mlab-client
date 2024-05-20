79b1bb1cf20236b8be1574126ee7a549328f0d87
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2024-05-20 00:47:39 +0000

    feat: implement ssh key gen for git server connection

--- pages/app/settings/configurations/index.tsx
+++ pages/app/settings/configurations/index.tsx
@@ -8,7 +8,6 @@ import { ClipboardIcon } from "../../../../components/icons/clipboard-icon";
 const Configurations: NextPage = () => {
 	const { user } = useUser();
 	const [pubKey, setPubKey] = React.useState("");
-	const [privKey, setPrivKey] = React.useState("");
 
 	React.useEffect(() => {
 		if (user === undefined) return;
@@ -17,7 +16,13 @@ const Configurations: NextPage = () => {
 		const fetchKeyPair = async () => {
 			try {
 				const res = await fetch(
-					`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/iam/ssh_key?user_id=${user.email}`
+					`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/iam/ssh_key?user_id=${user.email}`,
+					{
+						method: "GET",
+						headers: {
+							"Content-Type": "application/json",
+						},
+					}
 				);
 				if (res.status !== 200) {
 					throw {
@@ -29,9 +34,12 @@ const Configurations: NextPage = () => {
 
 				const data = await res.json();
 				setPubKey(data.pub_key);
-				setPrivKey(data.priv_key);
 			} catch (error: any) {
-				alert("Internal server error, contact admin");
+				if (error.status === 404) {
+					setPubKey("");
+				} else {
+					alert("Internal server error, contact admin");
+				}
 			}
 		};
 
@@ -39,7 +47,7 @@ const Configurations: NextPage = () => {
 	}, [user]);
 
 	const copyText = (id: string) => () => {};
-	const generateKeyPair = async () => {
+	const submitPubKey = async () => {
 		if (user === undefined) return;
 		try {
 			const res = await fetch(
@@ -62,7 +70,6 @@ const Configurations: NextPage = () => {
 			const data = await res.json();
 			console.log(data);
 			setPubKey(data.pub_key);
-			setPrivKey(data.priv_key);
 		} catch (error: any) {
 			alert("Internal server error, contact admin");
 		}
@@ -75,37 +82,25 @@ const Configurations: NextPage = () => {
 				className="flex flex-col h-auto box-border w-full p-5"
 				fullWidth={true}
 			>
-				<CardHeader className="text-white">SSH Key Pair</CardHeader>
+				<CardHeader className="text-white">
+					Update/Add New Public Key.{" "}
+					<a
+						className="text-primary"
+						target="_blank"
+						href="https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account"
+					>
+						&nbsp;[How to generate]
+					</a>
+				</CardHeader>
 				<CardBody className="flex flex-row w-full flex-nowrap">
 					{/* Two cols: 7/10-3/10 by ratio  */}
 					<div className="flex flex-col w-3/4 gap-4">
 						<Input
 							label="Public Key"
 							placeholder="Public Key"
-							endContent={
-								<button
-									className="focus:outline-none"
-									type="button"
-									onClick={copyText("pub_key")}
-								>
-									<ClipboardIcon />
-								</button>
-							}
 							type={"text"}
-						/>
-						<Input
-							label="Private Key"
-							placeholder="Private Key"
-							endContent={
-								<button
-									className="focus:outline-none"
-									type="button"
-									onClick={copyText("pub_key")}
-								>
-									<ClipboardIcon />
-								</button>
-							}
-							type={"text"}
+							value={pubKey}
+							onChange={(e) => setPubKey(e.target.value)}
 						/>
 					</div>
 					<div className="flex flex-col w-1/4 gap-4 px-5">
@@ -113,9 +108,9 @@ const Configurations: NextPage = () => {
 							size="lg"
 							type="button"
 							color="primary"
-							onClick={generateKeyPair}
+							onClick={submitPubKey}
 						>
-							(Re)Generate Key Pair
+							Update
 						</Button>
 					</div>
 				</CardBody>
